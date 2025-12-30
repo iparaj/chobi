14e57289f7d82a991ac335cac80bce839b45db4b
iparaj <gparaj@gmail.com>
2025-12-30 17:27:55 +0530

    run2

---
+++ data/albums.js
@@ -0,0 +1,14 @@
+const albums = [
+  {
+    id: "album-a",
+    title: "Album A",
+    cover: "images/album-a/a1.jpg",
+    images: ["a1.jpg", "a2.jpg", "a3.jpg"]
+  },
+  {
+    id: "album-b",
+    title: "Album B",
+    cover: "images/album-b/b1.jpg",
+    images: ["b1.jpg", "b2.jpg", "b3.jpg"]
+  }
+];
